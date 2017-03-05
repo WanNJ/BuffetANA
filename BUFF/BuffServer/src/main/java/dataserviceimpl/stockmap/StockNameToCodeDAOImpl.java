1e1941e2fab47d1853d50462c2d1c37c78a003ef
151250134 <151250134@smail.nju.edu.cn>
2017-03-05 21:49:56 +0800

    完成股票名到编码的映射接口

--- BUFF/BuffServer/src/main/java/dataserviceimpl/stockmap/StockNameToCodeDAOImpl.java
+++ BUFF/BuffServer/src/main/java/dataserviceimpl/stockmap/StockNameToCodeDAOImpl.java
@@ -2,14 +2,54 @@ package dataserviceimpl.stockmap;
 
 import dataservice.stockmap.StockNameToCodeDAO;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Set;
 
 /**
  * Created by slow_time on 2017/3/5.
  */
 public class StockNameToCodeDAOImpl implements StockNameToCodeDAO {
+
+    public static final String MAP_FILENAME = "/Users/slow_time/BuffettANA/Data/StockMap.csv";
+
     @Override
     public HashMap<String, String> getNameToCodeMap() {
-        return null;
+        HashMap<String, String> nameToCode = null;
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(MAP_FILENAME));
+            nameToCode = new HashMap<>();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] nameAndCode = line.split(",");
+                nameToCode.put(nameAndCode[0], nameAndCode[1]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return nameToCode;
+        }
+    }
+
+    public static void main(String[] args) {
+        StockNameToCodeDAO test = new StockNameToCodeDAOImpl();
+        HashMap<String, String> temp = test.getNameToCodeMap();
+        Set<String> name = temp.keySet();
+        for(String i : name) {
+            System.out.println(i + "," + temp.get(i));
+        }
     }
 }

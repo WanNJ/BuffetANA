4d733fed63cecc715d91d282b9f81cc3251b2bde
151250145 <151250145@smail.nju.edu.cn>
2017-03-08 11:19:34 +0800

    增加注解

--- BUFF/BuffPublic/src/main/java/vo/StockVolVO.java
+++ BUFF/BuffPublic/src/main/java/vo/StockVolVO.java
@@ -9,6 +9,13 @@ public class StockVolVO {
 
     public LocalDate date;
     public long vol;
+    /**
+     * add by wsw
+     *  这一天（周或月）的开盘价是否大于收盘价
+     *  如果 开盘价大于收盘价  返回true
+     *  如果 收盘价大于开盘价  返回false
+     *
+     */
     public boolean openAboveClose;
 
 

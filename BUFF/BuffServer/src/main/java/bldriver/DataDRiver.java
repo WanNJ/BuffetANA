635115f69d0dcc5725ecb34e089cda83e7280479
151250145 <151250145@smail.nju.edu.cn>
2017-03-15 15:08:05 +0800

    测试  搜索的文件驱动(主要复用田原代码)

---
+++ BUFF/BuffServer/src/main/java/bldriver/DataDRiver.java
@@ -0,0 +1,59 @@
+package bldriver;
+
+import blserviceimpl.singlestock.MALineServiceImpl;
+import dataservicestub.StockDaoImpl_stub;
+import po.StockPO;
+import util.DateUtil;
+import vo.MAPieceVO;
+
+import java.io.*;
+import java.rmi.RemoteException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by wshwbluebird on 2017/3/15.
+ */
+public class DataDRiver {
+    public static void main(String[] args) throws RemoteException {
+        String codeFile = "../Data/StockMap.csv";
+        generateStockPOs(codeFile);
+    }
+
+
+    private static void generateStockPOs(String fileName) {
+        BufferedReader br = null;
+        List<String> stringList = new ArrayList<>();
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] stockInfo = line.split(",");
+                String temp = stockInfo[1]+"("+stockInfo[0]+")";
+                stringList.add(temp);
+            }
+            //System.out.println(stringList.size());
+            List<String> str =stringList.parallelStream().filter(t->t.startsWith("1")).collect(Collectors.toList());
+            str.sort((a,b)->a.compareTo(b));
+            str.stream().limit(5).forEach(t-> System.out.println(t));
+
+        } catch (FileNotFoundException e) {
+            System.out.println(fileName + " is not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}

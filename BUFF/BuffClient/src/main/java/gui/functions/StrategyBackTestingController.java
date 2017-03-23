10dd37cba1820252c7113a60574285ab8efdcb31
151250212 <151250212@smail.nju.edu.cn>
2017-03-23 21:41:39 +0800

    增加回测界面原型

---
+++ BUFF/BuffClient/src/main/java/gui/functions/StrategyBackTestingController.java
@@ -0,0 +1,16 @@
+package gui.functions;
+
+import io.datafx.controller.FXMLController;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author zjy
+ */
+@FXMLController(value = "/resources/fxml/ui/StrategyBackTesting.fxml" , title = "StrategyBackTesting")
+public class StrategyBackTestingController {
+    @PostConstruct
+    public void init(){
+
+    }
+}

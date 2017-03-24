ae8ae9429684e82afa4a3e590dc3142103f660c7
151250134 <151250134@smail.nju.edu.cn>
2017-03-24 15:06:45 +0800

    增加股票池选择的VO

---
+++ BUFF/BuffPublic/src/main/java/stockenum/StockPool.java
@@ -0,0 +1,16 @@
+package stockenum;
+
+/**
+ * Created by slow_time on 2017/3/24.
+ * 股票池的所有类型
+ */
+public enum StockPool {
+    /**
+     * 所有股票
+     */
+    All,
+    /**
+     * 沪深300
+     */
+    HS300
+}
---
+++ BUFF/BuffPublic/src/main/java/vo/StockPoolConditionVO.java
@@ -0,0 +1,39 @@
+package vo;
+
+import stockenum.StockPool;
+
+import java.util.Set;
+
+/**
+ * Created by slow_time on 2017/3/24.
+ */
+public class StockPoolConditionVO {
+
+    /**
+     * 股票池的类型
+     */
+    public StockPool stockPool;
+    /**
+     * 所属板块的集合
+     */
+    public Set<String> block;
+    /**
+     * 所属行业的集合
+     */
+    public Set<String> industry;
+    /**
+     * 是否排除ST股票
+     */
+    public boolean excludeST;
+
+    public StockPoolConditionVO() {
+
+    }
+
+    public StockPoolConditionVO(StockPool stockPool, Set<String> block, Set<String> industry, boolean excludeST) {
+        this.stockPool = stockPool;
+        this.block = block;
+        this.industry = industry;
+        this.excludeST = excludeST;
+    }
+}

9533c8b40fb6c7e1dc4115049147adc130002c3c
151250212 <151250212@smail.nju.edu.cn>
2017-04-07 22:25:55 +0800

    增加几个输入

--- BUFF/BuffClient/src/main/java/gui/functions/StockChooseController.java
+++ BUFF/BuffClient/src/main/java/gui/functions/StockChooseController.java
@@ -1,9 +1,7 @@
 package gui.functions;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTabPane;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
+import gui.utils.DatePickerUtil;
 import io.datafx.controller.FXMLController;
 import javafx.fxml.FXML;
 import javafx.geometry.VPos;
@@ -13,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 
 import javax.annotation.PostConstruct;
@@ -23,16 +22,45 @@ import javax.annotation.PostConstruct;
 @FXMLController(value = "/resources/fxml/ui/StockChoose.fxml" , title = "StockChoose")
 public class StockChooseController {
 
+    @FXML private StackPane root;
     @FXML private JFXComboBox stockPool;//股票池
     @FXML private JFXComboBox plate;//板块
     @FXML private JFXComboBox industry;//行业
+    @FXML private JFXCheckBox ST;//是否排除ST
+    @FXML private JFXDatePicker from;//开始日期
+    @FXML private JFXDatePicker to;//结束日期
     @FXML private FlowPane quotaPane;//选股指标面板
     @FXML private JFXTabPane pickingConditions;//选股条件TabPane
     @FXML private GridPane filterCondition;//筛选条件GridPane
     @FXML private GridPane rankingCondition;//排名条件GridPane
+    @FXML private JFXComboBox<String> strategyType;//策略类型
+    @FXML private JFXTextField holdingPeriod;//持仓期
+    @FXML private JFXTextField formativePeriod;//形成期
+    @FXML private JFXTextField numOfShares;//持股数（持股比例）
 
     @PostConstruct
     public void init(){
+        //初始化界面用到的各种控件
+        from.setDialogParent(root);
+        to.setDialogParent(root);
+        //为日期选择器加上可选范围的控制
+        DatePickerUtil.initDatePicker(from,to);
+
+        addButtons();
+
+        strategyType.setOnAction(event -> {
+            if("均值策略".equals(strategyType.getValue())){
+                numOfShares.setPromptText("持股数");
+            }else if("动量策略".equals(strategyType.getValue())){
+                numOfShares.setPromptText("持股比例");
+            }
+        });
+    }
+
+    /**
+     * 添加选股指标的按钮
+     */
+    private void addButtons(){
         JFXButton[] buttons={new JFXButton("指标1"),new JFXButton("指标2"),new JFXButton("指标3"),
                 new JFXButton("指标4"),new JFXButton("指标5")};
         for(JFXButton button:buttons){
@@ -60,13 +88,16 @@ public class StockChooseController {
 
                     //添加比较符ComboBox和值TextField
                     JFXComboBox comparisonCharacter=new JFXComboBox();
+                    comparisonCharacter.setValue("小于");
                     comparisonCharacter.setMaxWidth(100);
                     JFXTextField value=new JFXTextField("250");
                     value.setMaxWidth(100);
                     filterCondition.add(comparisonCharacter,1,row);
                     filterCondition.add(value,3,row);
 
-                    delete.setOnAction(event1 -> filterCondition.getRowConstraints().remove(rowConstraints));
+                    delete.setOnAction(event1 -> {
+                        filterCondition.getRowConstraints().remove(rowConstraints);
+                    });
                     filterCondition.add(delete,4,row);
                 }else if ("排名条件".equals(pickingConditions.getSelectionModel().getSelectedItem().getText())){
                     rankingCondition.getRowConstraints().add(rowConstraints);
@@ -76,8 +107,10 @@ public class StockChooseController {
 
                     //添加次序ComboBox、范围ComboBox和权重TextField
                     JFXComboBox order=new JFXComboBox();
+                    order.setValue("从小到大");
                     order.setMaxWidth(100);
                     JFXComboBox range=new JFXComboBox();
+                    range.setValue("全部");
                     range.setMaxWidth(100);
                     JFXTextField weight=new JFXTextField("1");
                     weight.setMaxWidth(100);
@@ -93,4 +126,5 @@ public class StockChooseController {
         quotaPane.getChildren().addAll(buttons);
 
     }
+
 }

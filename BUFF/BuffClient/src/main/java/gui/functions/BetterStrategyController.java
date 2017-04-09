d97b0706bd3c713efb7181dfe56a53bc1a6e7664
151250212 <151250212@smail.nju.edu.cn>
2017-04-09 22:14:13 +0800

    调整布局

--- BUFF/BuffClient/src/main/java/gui/functions/BetterStrategyController.java
+++ BUFF/BuffClient/src/main/java/gui/functions/BetterStrategyController.java
@@ -1,6 +1,9 @@
 package gui.functions;
 
+import com.jfoenix.controls.JFXTreeTableView;
 import io.datafx.controller.FXMLController;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 
 import javax.annotation.PostConstruct;
 
@@ -10,6 +13,13 @@ import javax.annotation.PostConstruct;
 
 @FXMLController(value = "/resources/fxml/ui/BetterStrategy.fxml" , title = "EstimateStrategy")
 public class BetterStrategyController {
+
+    @FXML JFXTreeTableView treeTableView_formativePeriod;//固定形成期的TreeTableView
+    @FXML LineChart excessEarnings_formativePeriod;//固定形成期的超额收益
+    @FXML LineChart winningStrategies_formativePeriod;//固定形成期的策略胜率
+    @FXML JFXTreeTableView treeTableView_holdingPeriod;//固定持有期的TreeTableView
+    @FXML LineChart excessEarnings_holdingPeriod;//固定持有期的超额收益
+    @FXML LineChart winningStrategies_holdingPeriod;//固定持有期的策略胜率
 
     @PostConstruct
     public void init(){

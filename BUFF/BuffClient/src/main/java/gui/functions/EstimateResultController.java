6d69920fffe48c1a30abea3d7f6596027401ad75
151250212 <151250212@smail.nju.edu.cn>
2017-04-09 21:52:47 +0800

    前两张图的布局调整

--- BUFF/BuffClient/src/main/java/gui/functions/EstimateResultController.java
+++ BUFF/BuffClient/src/main/java/gui/functions/EstimateResultController.java
@@ -1,5 +1,6 @@
 package gui.functions;
 
+import com.jfoenix.controls.JFXTreeTableView;
 import io.datafx.controller.FXMLController;
 import io.datafx.controller.flow.Flow;
 import io.datafx.controller.flow.FlowException;
@@ -7,8 +8,10 @@ import io.datafx.controller.flow.FlowHandler;
 import io.datafx.controller.flow.context.FXMLViewFlowContext;
 import io.datafx.controller.flow.context.ViewFlowContext;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import javax.annotation.PostConstruct;
 
@@ -19,29 +22,22 @@ import javax.annotation.PostConstruct;
 @FXMLController(value = "/resources/fxml/ui/EstimateResult.fxml" , title = "EstimateResult")
 public class EstimateResultController {
 
-    @FXMLViewFlowContext
-    private ViewFlowContext context;
+    @FXMLViewFlowContext private ViewFlowContext context;
 
-    @FXML
-    BorderPane barpieBorder;  //画收益分布的直方图和饼图
+    @FXML VBox vBox;
+    @FXML JFXTreeTableView treeTableView;
+    @FXML LineChart chart;
 
     private FlowHandler incomeBarPieHandler; //画收益分布的直方图和饼图的handler
-
     private StackPane incomeBarPiePane; //画收益分布的直方图和饼图的pane
 
     @PostConstruct
     public void init() throws FlowException {
 
-
         Flow incomeBArPieFlow = new Flow(IncomeBarPieController.class);
-
-
         incomeBarPieHandler = incomeBArPieFlow.createHandler(context);
-
         context.register("incomeBarPieHandler",incomeBarPieHandler);
-
         incomeBarPiePane = incomeBarPieHandler.start();
-
-        barpieBorder.centerProperty().setValue(incomeBarPiePane);
+        vBox.getChildren().addAll(incomeBarPiePane);
     }
 }
--- BUFF/BuffClient/src/main/java/gui/functions/StrategyBackTestingController.java
+++ BUFF/BuffClient/src/main/java/gui/functions/StrategyBackTestingController.java
@@ -66,7 +66,7 @@ public class StrategyBackTestingController {
         drawer.setSidePane(sideMenuFlowHandler.start(new AnimatedFlowContainer(Duration.millis(320), ContainerAnimations.SWIPE_LEFT)));
 
         viewsBox.getChildren().addAll(new Flow(EstimateResultController.class).createHandler(context).start());
-        viewsBox.getChildren().addAll(new Flow(BetterStrategyController.class).createHandler(context).start());
+        //viewsBox.getChildren().addAll(new Flow(BetterStrategyController.class).createHandler(context).start());
         viewsBox.getChildren().addAll(new Flow(EstimateStrategyController.class).createHandler(context).start());
 
     }

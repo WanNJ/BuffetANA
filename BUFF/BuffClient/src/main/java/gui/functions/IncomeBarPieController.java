5bff49732fbb89e1770c1222fb337bd7bb5daa78
151250135 <151250135@smail.nju.edu.cn>
2017-04-14 16:09:33 +0800

    为相对收益分布图做准备

--- BUFF/BuffClient/src/main/java/gui/functions/IncomeBarPieController.java
+++ BUFF/BuffClient/src/main/java/gui/functions/IncomeBarPieController.java
@@ -1,15 +1,27 @@
 package gui.functions;
 
 import io.datafx.controller.FXMLController;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.StackPane;
 
 import javax.annotation.PostConstruct;
 
-/**
- * Created by wshwbluebird on 2017/3/24.
- */
-
 @FXMLController(value = "/resources/fxml/ui/IncomeBarPie.fxml" , title = "IncomeBarPie")
 public class IncomeBarPieController {
+    @FXML
+    private StackPane peiChartPane;
+
+    @FXML
+    private BarChart<?, ?> barChart;
+
+    @FXML
+    private StackPane barChartPane;
+
+    @FXML
+    private PieChart pieChart;
+
     @PostConstruct
     public void init(){
 

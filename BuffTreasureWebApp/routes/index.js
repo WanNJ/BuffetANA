42a823331bd1091fdc03a1e0ca75602826c7e195
151250134 <151250134@smail.nju.edu.cn>
2017-05-07 21:17:37 +0800

    删掉多余的代码

--- BuffTreasureWebApp/routes/index.js
+++ BuffTreasureWebApp/routes/index.js
@@ -63,14 +63,6 @@ router.route('/sign-in')
                 }
             }
         });
-        require('../models/singleStock.js').singleStockDB.getStockInfoByCode('000001', (err, docs) => {
-           if (err) {
-               console.log(err);
-           }
-           else {
-               console.log(docs.length);
-           }
-        });
     });
 
 router.get('/logout', function(req, res, next){

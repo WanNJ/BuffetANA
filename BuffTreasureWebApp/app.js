50a6e4a5016127d45bcdf6fb7aca8358c4cba244
151250135 <151250135@smail.nju.edu.cn>
2017-05-08 23:06:39 +0800

    再次完善用户系统，完善用户体验

--- BuffTreasureWebApp/app.js
+++ BuffTreasureWebApp/app.js
@@ -75,7 +75,7 @@ app.use((err, req, res, next) => {
 });
 
 // 数据库连接 MongoDB
-mongoose.connect('mongodb://192.168.1.102/formal');
+mongoose.connect('mongodb://192.168.1.103/formal');
 
 mongoose.connection.on('open', function () {
     console.log('Connected to Mongoose');
--- BuffTreasureWebApp/routes/index.js
+++ BuffTreasureWebApp/routes/index.js
@@ -3,6 +3,17 @@ let userService = require('../bl/userbl');
 let router = express.Router();
 
 /* GET HOME PAGE 'next'在下面的工作中可能会用到 */
+router.use((req, res, next) => {
+    if (req.session.alertType !== undefined) {
+        res.locals.alertType = req.session.alertType;
+        res.locals.alertMessage = req.session.alertMessage;
+
+        req.session.alertType = undefined;
+        req.session.alertMessage = undefined;
+    }
+    next();
+});
+
 router.get('/', function (req, res, next) {
     res.render('index');
 });
@@ -32,10 +43,9 @@ router.route('/sign-in')
     .get(function (req, res, next) {
         res.render('sign-in');
     })
-    .post(function(req, res, next){
+    .post(function (req, res, next) {
         userService.login(req.body.username, req.body.password, (err, status) => {
-            if(err) {
-                //TODO 数据库链接错误页面渲染
+            if (err) {
                 res.locals.message = err.message;
                 res.locals.error = req.app.get('env') === 'development' ? err : {};
                 // 提交错误页
@@ -43,32 +53,34 @@ router.route('/sign-in')
                 res.render('error');
             }
 
-            if(status === false) {
-                //TODO 渲染密码错误页面
-                req.locals.alertType = "alert-danger";
-                res.locals.message = "账号或密码错误，请重试";
-                res.redirect('/sign-in');
+            if (status === false) {
+                res.locals.alertType = "alert-danger";
+                res.locals.alertMessage = "账号或密码错误，请重试";
+
+                res.render('sign-in');
             } else {
                 let sess = req.session;
 
-                if(sess.user){
-                    req.locals.alertType = "alert-warning";
-                    req.locals.alertMessage = "您已经过了，请勿重复登录。";
+                if (sess.user) {
+                    //重复登录，理论上不可能，但要防止攻击
+                    req.session.alertType = "alert-warning";
+                    req.session.alertMessage = "请勿重复登录！";
                     res.redirect('/');
                 } else {
-                    //TODO 渲染登录成功页面
+                    //登录成功
                     req.session.user = req.body.username;
-                    req.locals.alertType = "alert-success";
-                    req.locals.alertMessage = "恭喜您登录成功！";
+
+                    req.session.alertType = "alert-success";
+                    req.session.alertMessage = "恭喜您登录成功！";
                     res.redirect('/');
                 }
             }
         });
     });
 
-router.get('/logout', function(req, res, next){
-    req.session.destroy(function(err) {
-        if(err){
+router.get('/logout', function (req, res, next) {
+    req.session.destroy(function (err) {
+        if (err) {
             res.locals.message = err.message;
             res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -78,9 +90,6 @@ router.get('/logout', function(req, res, next){
         }
 
         res.clearCookie("BuffSession");
-
-        req.locals.alertType = "alert-success";
-        req.locals.alertMessage = "已成功登出！";
         res.redirect('/');
     });
 });
@@ -90,25 +99,32 @@ router.route('/sign-up')
         res.render('sign-up');
     })
     .post(function (req, res, next) {
+        /** @namespace req.body.confirm_password */
+        if(req.body.password !== req.body.confirm_password){
+            res.locals.alertType = "alert-danger";
+            res.locals.alertMessage = "两次输入的密码不一致！";
+            res.render('sign-up');
+        }
+
         userService.signUp(req.body.username, req.body.password, req.body.email, (err, status) => {
-            if(err) {
+            if (err) {
                 res.locals.message = err.message;
                 res.locals.error = req.app.get('env') === 'development' ? err : {};
                 // 提交错误页
                 res.status(err.status || 500);
                 res.render('error');
             }
-            if(status === false) {
-                //TODO 重名渲染页面
-                req.locals.alertType = "alert-danger";
-                req.locals.alertMessage = "该用户名已存在，请更换您的用户名！";
-                res.redirect('/sign-up');
-            }else {
+            if (status === false) {
+                //重名渲染页面
+                res.locals.alertType = "alert-warning";
+                res.locals.alertMessage = "该用户名已存在，请更换您的用户名！";
+                res.render('sign-up');
+            } else {
                 req.session.user = req.body.username;
 
-                //TODO 注册成功渲染页面
-                req.locals.alertType = "alert-success";
-                res.locals.alertMessage = "恭喜您注册成功！";
+                //注册成功渲染页面
+                req.session.alertType = "alert-success";
+                req.session.alertMessage = "恭喜您注册成功！系统已经帮你自动登录！";
                 res.redirect('/');
             }
         });

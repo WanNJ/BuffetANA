12d8ae531373806c941d08fec18084541436814f
151250135 <151250135@smail.nju.edu.cn>
2017-05-17 15:22:29 +0800

    个股界面性能优化

--- BuffTreasureWebApp/bl/allStockbl.js
+++ BuffTreasureWebApp/bl/allStockbl.js
@@ -21,3 +21,4 @@ exports.getAllStockCodeAndName = (callback) => {
         }
     });
 };
+
--- BuffTreasureWebApp/routes/singleStock.js
+++ BuffTreasureWebApp/routes/singleStock.js
@@ -1,41 +1,50 @@
 let express = require('express');
+let allStockBl = require('../bl/allStockbl');
 let singleStockService = require('../bl/singleStockbl');
 let router = express.Router();
 
-/* GET users listing. */
-router.post('/getDailyKLine', function(req, res, next) {
-    let re = /(.*)\(([0-9]+)\)/;
+router.get('/getAllStockList', (req, res, next) => {
+    allStockBl.getAllStockCodeAndName((err, allStocks) => {
+        if (err)
+            res.render('error');
+
+        res.send(allStocks);
+    });
+});
+
+router.post('/getDailyKLine', (req, res, next) => {
+    let re = /([0-9]+)\((.*)\)/;
     let result = re.exec(req.body.query);
-    let stockCode = result[2];
+    let stockCode = result[1];
 
     singleStockService.getDailyData(stockCode, (err, docs) => {
-        if(err)
-            render('error');
+        if (err)
+            res.render('error');
 
         res.send(docs);
     });
 });
 
-router.post('/getWeeklyKLine', function(req, res, next) {
-    let re = /(.*)\(([0-9]+)\)/;
+router.post('/getWeeklyKLine', (req, res, next) => {
+    let re = /([0-9]+)\((.*)\)/;
     let result = re.exec(req.body.query);
-    let stockCode = result[2];
+    let stockCode = result[1];
 
     singleStockService.getWeeklyData(stockCode, (err, docs) => {
-        if(err)
-            render('error');
+        if (err)
+            res.render('error');
 
         res.send(docs);
     });
 });
 
-router.post('/getMonthlyKLine', function(req, res, next) {
-    let re = /(.*)\(([0-9]+)\)/;
+router.post('/getMonthlyKLine', (req, res, next) => {
+    let re = /([0-9]+)\((.*)\)/;
     let result = re.exec(req.body.query);
-    let stockCode = result[2];
+    let stockCode = result[1];
 
     singleStockService.getMonthlyData(stockCode, (err, docs) => {
-        if(err)
+        if (err)
             render('error');
 
         res.send(docs);

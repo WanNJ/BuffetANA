c0ff258ae94a42c087e58e004a5a6d2047bf2ecf
151250134 <151250134@smail.nju.edu.cn>
2017-06-03 10:32:18 +0800

    加了注释

--- BuffTreasureWebApp/test/testPython/execPython_file.js
+++ BuffTreasureWebApp/test/testPython/execPython_file.js
@@ -3,6 +3,7 @@ let arg1 = 'hello';
 let arg2 = 'world';
 const filename = 'test_py.py';
 
+// 与命令行中执行python所敲的命令一样，如果你有alias，把python3换成对应的alias就可以了
 exec('python3' + ' ' + filename + ' ' + arg1 + ' ' + arg2, function(err, stdout, stderr){
     if(err) {
         console.log('stderr',err);

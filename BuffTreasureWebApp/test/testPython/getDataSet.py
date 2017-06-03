41ef26dc2cec87fe2f759ed4fb3e89bdd17dc6bf
151250145 <151250145@smail.nju.edu.cn>
2017-06-03 19:00:16 +0800

    搞定了 最简单的模型 剩下的就更简单了

---
+++ BuffTreasureWebApp/test/testPython/getDataSet.py
@@ -0,0 +1,111 @@
+import datetime
+import pymongo
+import numpy as np
+import csv
+
+
+def connect_mongodb():
+    servers = "mongodb://localhost:27017"
+    conn = pymongo.MongoClient(servers)
+    print(conn.database_names())
+    db = conn.allInfo
+    return db
+
+def pre_deal_data(code , holdingPeriods):
+    pro = ['afterAdjClose', 'date', 'turnOverRate', 'changePrice', 'volume', 'close', 'low', 'high',
+            'changeRate', 'beforeAdjClose']
+    propre = ['afterAdjClose', 'turnOverRate', 'changePrice', 'volume', 'close', 'low', 'high',
+             'changeRate', 'beforeAdjClose']
+
+    heads = []
+
+    label0 = {'less10':1,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label1 = {'less10':0,'10-7.5':1,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label2 = {'less10':0,'10-7.5':0,'7.5-5':1,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label3 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':1,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label4 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':1,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label5 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':1,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label6 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':1,'5.5-7.5':0,'7.5-10':0,'more10':0}
+    label7 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':1,'7.5-10':0,'more10':0}
+    label8 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':1,'more10':0}
+    label9 = {'less10':0,'10-7.5':0,'7.5-5':0,'5-2.5':0,'2.5-0':0,'0-2.5':0,'2.5-5.5':0,'5.5-7.5':0,'7.5-10':0,'more10':1}
+
+    labelhead = list(label0.keys())
+    labels = [label0,label1,label2,label3,label4,label5,label6,label7,label8,label9]
+    labelset = []
+
+    for i in range(10):
+        for st in propre:
+            heads.append(st+str(i))
+    print(heads)
+
+    db = connect_mongodb()
+    collection = db[code]
+    beginDate = datetime.datetime.strptime('2015-04-05', '%Y-%m-%d')
+    cc = collection.find({'volume': {'$gt': 0}, 'date': {'$gt': beginDate}}, pro).sort('date',pymongo.ASCENDING)
+
+    nor = {}
+
+    data = []
+    for o in cc:
+        o.pop('_id')
+        data.append(o)
+
+    js = 0
+    temp = data[0]
+    for key in list(temp.keys()):
+        nor[key] =[]
+    for o in data:
+        for key in list(o.keys()):
+            nor[key].append(o[key])
+        js += 1
+
+    for o in data:
+        for key in list(o.keys()):
+            if key!='date':
+                o[key] = (o[key]-np.mean(nor[key]))/np.std(nor[key])
+
+    with open('dataset.csv', 'w', encoding='gbk') as f1:
+        fieldnames = heads
+        writer = csv.DictWriter(f1, fieldnames=fieldnames)
+        writer.writeheader()
+        content = {}
+        for i in range(holdingPeriods,len(data) - holdingPeriods):
+            change = 100*(data[i+9]['close']-data[i]['close'])/data[i]['close']
+            ll = int((change+10)/2.5)+1
+            if ll < 0:
+                ll = 0
+            if ll > 9:
+                ll = 9
+            #print(ll)
+            label = labels[ll]
+            labelset.append(label)
+            proind = 0
+            for ind in range(i,i-10,-1):
+                value = data[ind]
+                for js in range(len(propre)):
+                    head = heads[proind]
+                    content[head] = value[head[:-1]]
+                    proind +=1
+            writer.writerow(content)
+
+    with open('labelset.csv', 'w', encoding='gbk') as f1:
+        fieldnames = labelhead
+        writer = csv.DictWriter(f1, fieldnames=fieldnames)
+        writer.writeheader()
+        for l in labelset:
+            writer.writerow(l)
+
+    with open('predict.csv', 'w', encoding='gbk') as f1:
+        l = len(data)-30
+        fieldnames = heads
+        writer = csv.DictWriter(f1, fieldnames=fieldnames)
+        writer.writeheader()
+        proind = 0
+        for ind in range(l-1, l - 10, -1):
+            value = data[ind]
+            for js in range(len(propre)):
+                head = heads[proind]
+                content[head] = value[head[:-1]]
+                proind += 1
+        writer.writerow(content)
---
+++ BuffTreasureWebApp/test/testPython/testEasyModel.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wshwbluebird on 2017/6/3.
+ */
+const exec = require('child_process').exec;
+let arg1 = '000001';
+let arg2 = '10';
+const filename = 'testModel.py';
+
+// 与命令行中执行python所敲的命令一样，如果你有alias，把python3换成对应的alias就可以了
+exec('python3' + ' ' + filename + ' ' + arg1 + ' ' + arg2, function(err, stdout, stderr){
+    if(err) {
+        console.log('stderr',err);
+    }
+    if(stdout) {
+        console.log(stdout);
+    }
+
+});

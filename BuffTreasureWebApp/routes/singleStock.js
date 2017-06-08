5063dbe157815ec4e1c433561a6ff9b23f65f576
151250135 <151250135@smail.nju.edu.cn>
2017-06-08 11:04:19 +0800

    完成个股右边栏

--- BuffTreasureWebApp/routes/singleStock.js
+++ BuffTreasureWebApp/routes/singleStock.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let allStockBl = require('../bl/allStockbl');
 let singleStockService = require('../bl/singleStockbl');
+let industryService = require('../bl/industryandbench/industrybl');
 let userBl = require('../bl/userbl');
 let router = express.Router();
 
@@ -19,6 +20,24 @@ router.get('/getHotStockList', (req, res, next) => {
             res.render('error');
         else
             res.send(infos);
+    });
+});
+
+router.get('/getStockIndustry', (req, res, next) => {
+    industryService.getIndustryByCode(req.query.stockCode, (err, indus) => {
+        if (err)
+            res.render('error');
+        else
+            res.send(indus);
+    });
+});
+
+router.get('/getStockBoard', (req, res, next) => {
+    industryService.getBoardsByCode(req.query.stockCode, (err, boards) => {
+        if (err)
+            res.render('error');
+        else
+            res.send(boards);
     });
 });
 

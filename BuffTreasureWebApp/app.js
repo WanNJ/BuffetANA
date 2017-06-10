221cb2fe073c8a83bc28e53c0570a6b577fefccf
151250212 <151250212@smail.nju.edu.cn>
2017-06-10 16:36:04 +0800

    添加推荐股票的按钮响应

--- BuffTreasureWebApp/app.js
+++ BuffTreasureWebApp/app.js
@@ -80,7 +80,7 @@ app.use((err, req, res, next) => {
 // 为了解决DeprecationWarning: Mongoose: mpromise (mongoose's default promise library) is deprecated
 mongoose.Promise = global.Promise;
 // 数据库连接 MongoDB
-mongoose.connect('mongodb://172.28.179./allInfo');
+mongoose.connect('mongodb://172.28.179.189/allInfo');
 
 mongoose.connection.on('open', function () {
     console.log('Connected to Mongoose');
--- BuffTreasureWebApp/bl/functionMap/storeMap.js
+++ BuffTreasureWebApp/bl/functionMap/storeMap.js
@@ -9,7 +9,7 @@ exports.chmap ={
     '最低价' :'low',
     '流通市值': 'floatMarketValue',
     '换手率': 'turnOverRate',
-    '复权收盘价' :'adjClose',
+    '复权收盘价' :'afterAdjClose',
     '开盘价' :'open',
     '收盘价': 'close',
     '市值' : 'marketValue'
